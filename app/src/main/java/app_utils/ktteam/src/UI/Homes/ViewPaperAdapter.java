874bcb0718a6c96d6f408e8f0a54208f3197a27a
Khang@22 <nguyenhoangkhangnsc19@gmail.com>
2022-12-31 22:35:52 +0700

    create flow ui main

---
+++ app/src/main/java/app_utils/ktteam/src/UI/Homes/ViewPaperAdapter.java
@@ -0,0 +1,39 @@
+package app_utils.ktteam.src.UI.Homes;
+
+import androidx.annotation.NonNull;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentManager;
+import androidx.fragment.app.FragmentStatePagerAdapter;
+
+import app_utils.ktteam.src.UI.Homes.Fragments.HistoryFragment;
+import app_utils.ktteam.src.UI.Homes.Fragments.HomeFragment;
+import app_utils.ktteam.src.UI.Homes.Fragments.ProfileFragment;
+
+public class ViewPaperAdapter extends FragmentStatePagerAdapter {
+
+    public ViewPaperAdapter(@NonNull FragmentManager fm, int behavior) {
+        super(fm, behavior);
+    }
+
+    @NonNull
+    @Override
+    public Fragment getItem(int position) {
+        switch (position)
+        {
+            case 0:
+                return new HomeFragment();
+            case 1:
+                return  new HistoryFragment();
+            case 2:
+                return new ProfileFragment();
+            default:
+                return new HomeFragment();
+        }
+
+    }
+
+    @Override
+    public int getCount() {
+        return 3; // số lượng fragment
+    }
+}
